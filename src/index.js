c60b4e23619f4d2e259fa3b73c7c87bfa8a0165a
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-15 15:09:16 +0300

    init: if number < 100
    init: if number < 100

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 module.exports = function toReadable (number) {
-    if (number === 0 ) {
-        return "zero";
-    }
-    let nums = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    if (number <= nums.length - 1) {
-        return nums[number];
-    }
+	if (number === 0 ) {
+		return "zero";
+	}
+
+	let nums = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+	let numty = ["" ,"","twenty ","thirty ","forty ","fifty ","sixty ","seventy ","eighty ","ninety "]
+
+	if (number < 20) {
+		return nums[number];
+	}
+
+	number = number.toString().split("");
+
+	if (number.length === 2) {
+		return (numty[number[0]] + nums[+number[1]])
+	}
 }
