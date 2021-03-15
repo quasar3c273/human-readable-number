394c58f2ffa28a34029178ee4793750836ef8d6a
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-15 15:44:46 +0300

    feat: add hundred number
    feat: add hundred number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-module.exports = function toReadable (number) {
-	if (number === 0 ) {
+module.exports = function toReadable(number) {
+	if (number === 0) {
 		return "zero";
 	}
 
 	let nums = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-	let numty = ["" ,"","twenty ","thirty ","forty ","fifty ","sixty ","seventy ","eighty ","ninety "]
+	let numty = ["", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "]
 
 	if (number < 20) {
 		return nums[number];
@@ -13,6 +13,14 @@ module.exports = function toReadable (number) {
 	number = number.toString().split("");
 
 	if (number.length === 2) {
-		return (numty[number[0]] + nums[+number[1]])
+		return (numty[number[0]] + nums[+number[1]]).trim()
+	} else if (number.length === 3) {
+		if (number[1] === "0" && number[2] === "0") {
+			return nums[+number[0]] + " hundred"
+		} else if (number[1] === "1") {
+			return nums[+number[0]] + " hundred " + nums[+(number[1] + number[2])];
+		} else {
+			return (nums[+number[0]] + " hundred " + numty[number[1]] + nums[+number[2]]).trim();
+		}
 	}
 }
