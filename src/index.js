70273a012a75299cf096a56e6bf944b8c1685fc8
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-15 14:44:14 +0300

    init: start task human-readable-number
    init: if (num < 20)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0 ) {
+        return "zero";
+    }
+    let nums = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    if (number <= nums.length - 1) {
+        return nums[number];
+    }
 }
